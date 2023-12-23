cbf4718ca6238b4d64be9d9e10ceba29155682dd
daikiku10 <soccerdk19.910@gmail.com>
2023-12-23 23:24:28 +0900

    ログインフォームのクリック処理の形づくり

---
+++ src/components/organisms/LoginForm/LoginForm.css.ts
@@ -0,0 +1,9 @@
+import { style } from "@vanilla-extract/css";
+
+export const textField = style({
+  marginBottom: "24px",
+});
+
+export const button = style({
+  backgroundColor: "orange",
+});
--- src/components/organisms/LoginForm/LoginForm.tsx
+++ src/components/organisms/LoginForm/LoginForm.tsx
@@ -2,18 +2,37 @@
 
 import { Button } from "@/components/atoms/Button";
 import { Text } from "@radix-ui/themes";
+import * as styles from "./LoginForm.css";
+import { registerLoginUser } from "./hooks";
+import { useRouter } from "next/navigation";
 
 /**
  * ログインフォーム
  * @returns コンポーネント
  */
 const LoginForm = () => {
+  const router = useRouter();
   return (
     <div>
-      <Text>なまえを入力</Text>
-      <Button onClick={() => console.log("クリック")}>
-        チャットをはじめる
-      </Button>
+      <form
+        action="/chat"
+        onSubmit={(e) => {
+          e.preventDefault();
+          registerLoginUser();
+          router.push("/chat");
+        }}
+      >
+        <div className={styles.textField}>
+          {/* TODO: テキストフィールドにする */}
+          <Text>なまえを入力</Text>
+        </div>
+
+        <div>
+          <Button className={styles.button} type="submit" size="4">
+            チャットをはじめる
+          </Button>
+        </div>
+      </form>
     </div>
   );
 };
